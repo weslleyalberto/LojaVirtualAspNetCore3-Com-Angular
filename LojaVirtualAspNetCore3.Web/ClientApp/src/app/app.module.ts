a4cece6669ed2699b2fd91abe6bf9dd50488b169
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2019-11-03 08:09:21 -0300

    Validação formato Angular Bootstrap

--- LojaVirtualAspNetCore3.Web/ClientApp/src/app/app.module.ts
+++ LojaVirtualAspNetCore3.Web/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ProdutoComponet } from './produto/produto.componet';
+import { LoginComponent } from './usuario/login/login.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ProdutoComponet } from './produto/produto.componet';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    ProdutoComponet
+    ProdutoComponet,
+    LoginComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,7 +30,8 @@ import { ProdutoComponet } from './produto/produto.componet';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
         { path: 'fetch-data', component: FetchDataComponent },
-        { path: 'produto', component: ProdutoComponet }
+        { path: 'produto', component: ProdutoComponet },
+        { path: 'entrar', component: LoginComponent } 
     ])
   ],
   providers: [],
--- LojaVirtualAspNetCore3.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ LojaVirtualAspNetCore3.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,1 +1,26 @@
-﻿
+import { Component } from "@angular/core";
+import { Usuario } from "../../model/usuario";
+
+@Component({
+    selector: "app-login",
+    templateUrl: "./login.component.html",
+    styleUrls : ["./login.component.css"]
+})
+export class LoginComponent {
+    public usuario = new Usuario();
+    public usuarioAutenticado: boolean = false;
+    
+    constructor() {
+        this.usuario = new Usuario();
+    }
+   
+    
+    entrar() {
+        if (this.usuario.email == "wes@wes.com" && this.usuario.senha == "12345") {
+            this.usuarioAutenticado = true;
+        }
+       
+    }
+    
+
+}
